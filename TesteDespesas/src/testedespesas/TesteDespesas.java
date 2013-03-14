ead4c41e2111f2f56d37dc10390b900e8fe8e8c0
Abilio <Abilio@MZPC>
2013-03-14 19:15:39 +0000

    proj v2

--- TesteDespesas/src/testedespesas/TesteDespesas.java
+++ TesteDespesas/src/testedespesas/TesteDespesas.java
@@ -1,19 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package testedespesas;
 
-/**
- *
- * @author MNLyle
- */
+
 public class TesteDespesas {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+   
     }
 }
