0f55fe9867a937273c07d730dda53b02f9e342f9
MNLyle <MNLyle@WTFAYTA-FS>
2013-03-15 23:18:16 +0000

    Projecto v1.0

--- TesteDespesas/src/testedespesas/TesteDespesas.java
+++ TesteDespesas/src/testedespesas/TesteDespesas.java
@@ -6,9 +6,6 @@ public class TesteDespesas {
 
    
     public static void main(String[] args) {
-        int alteração;
-        System.out.println("ola");
-        //durães
-        // Março
+     
     }
 }
