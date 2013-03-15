5520ffc5bfa1596bb4e876dda5a9a46743e6e2a0
MNLyle <MNLyle@WTFAYTA-FS>
2013-03-15 23:28:56 +0000

    Projecto v1.1

--- TesteDespesas/src/testedespesas/Menu.java
+++ TesteDespesas/src/testedespesas/Menu.java
@@ -10,4 +10,27 @@ package testedespesas;
  */
 public class Menu {
     
+    public Menu(){
+        System.out.println("Menu:\n Intrduza a sua opção"); 
+        System.out.println("[1] Registar Desesa"); 
+        System.out.println("[0] Sair"); 
+        
+        System.out.println("Selection: "); 
+        int selection=scanner.nextInt();     
+       
+       switch (selection){
+             
+           case 1:System.out.println("Registar Desesa");
+                break;
+         
+             
+        
+           case 0:System.out.println("Exit Successful");
+                System.exit(0);
+                        
+           default:System.out.println("Por favor introduza uma opção válida:");
+           
+       };
+        
+    }
 }
